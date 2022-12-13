be55a8a10e9cedc52592d065c370372964445f8c
aneblaga <ane.blaga@yahoo.com>
2022-12-13 17:10:39 +0200

    commit lab10

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -18,6 +18,7 @@ namespace ane_blaga_lab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
 
         }
         public Task<List<ShopList>> GetShopListsAsync()
@@ -83,6 +84,21 @@ namespace ane_blaga_lab7.Data
             + " inner join ListProduct LP"
             + " on P.ID = LP.ProductID where LP.ShopListID = ?",
             shoplistid);
+        }
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
         }
     }
 }
--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace ane_blaga_lab7.Models
 {
@@ -14,6 +15,10 @@ namespace ane_blaga_lab7.Models
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
+
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
+
 
     }
 }
